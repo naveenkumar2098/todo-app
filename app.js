ebadfa183e068fc7c88857729ea3ea440edc7fa3
naveenkumar2098 <naveenkumar2098>
2019-10-15 12:28:22 +0530

    Modified app.js and index.ejs

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('view engine','ejs');
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({
+    extended : false
+}));
+
+app.use(express.static('assets'));
+
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 4040;
 
